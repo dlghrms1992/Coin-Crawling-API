571adb5ace55de84dfa95104b6bf52e98ad155fe
GoGoTyping <ihogeun51@gmail.com>
2023-09-21 13:44:49 +0900

    properties url 오타 수정 및 url list 조회

--- src/main/java/com/coin/api/controller/CrawlingController.java
+++ src/main/java/com/coin/api/controller/CrawlingController.java
@@ -1,23 +1,30 @@
 package com.coin.api.controller;
 
 
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.annotation.Documented;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/")
 public class CrawlingController {
 
     @Value("${crawling.url}")
     private List<String> urlList = new ArrayList<>();
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    @GetMapping("/g")
     private String googleCrawing(){
 
+        Document doc = new Document(null);
         return "";
     }
 
