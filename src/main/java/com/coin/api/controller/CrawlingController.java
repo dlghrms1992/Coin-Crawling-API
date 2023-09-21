b778b313b995b6d0133721bc7a9d921a856ed51c
GoGoTyping <ihogeun51@gmail.com>
2023-09-21 15:29:56 +0900

    properties url 오타 수정 및 url list 조회

--- src/main/java/com/coin/api/controller/CrawlingController.java
+++ src/main/java/com/coin/api/controller/CrawlingController.java
@@ -1,15 +1,17 @@
 package com.coin.api.controller;
 
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.annotation.Documented;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,13 +21,24 @@ public class CrawlingController {
 
     @Value("${crawling.url}")
     private List<String> urlList = new ArrayList<>();
-    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Value("${https://www.google.com/search?q=}")
+    private String googleSearchUrl;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/g")
-    private String googleCrawing(){
+    private String googleCrawing(@RequestParam("query") String query) throws IOException {
 
+
+        logger.debug("get query -> {}", query);
         Document doc = new Document(null);
-        return "";
+        if(query == null){
+            doc = Jsoup.connect(googleSearchUrl).get();
+        }else {
+            doc = Jsoup.connect(googleSearchUrl + query).get();
+        }
+        return doc.toString();
     }
 
 
